96631868f1a451f13ccd1f88d99a1f76aa919d77
bit <bit@DESKTOP-DMTFKMA>
2019-05-21 20:46:19 +0900

    0521

---
+++ 0521work/src/mainClass1.java
@@ -0,0 +1,16 @@
+
+public class mainClass1 {
+
+	public static void main(String[] args) {
+		int[] arr = { 10, 20, 30, 40, 50 };
+		int sum = 0;
+		
+		for (int i = 0; i < arr.length; i++) {
+			sum = arr[i] + sum;
+			
+		}
+		
+		System.out.println("sum = " + sum);
+	}
+
+}
